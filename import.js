3e0ee4f7b62d54bc5aeb0ecf98bff3e2c6d7ad1c
Alexander Sologub <alexander.sologub@rightscale.com>
2014-11-18 18:34:35 +0300

    Meter Collections: Helpers for them

---
+++ import.js
@@ -0,0 +1,16 @@
+
+// db.createCollection('projects');
+// db.createCollection('directories');
+// db.createCollection('files');
+
+// db.projects.insert({name: 'TestProject'});
+// testProject = db.projects.findOne({name: 'TestProject'});
+
+// db.directories.insert({name: 'TestProjectRoot'});
+// testProjectRoot = db.directories.findOne({name: 'TestProjectRoot'});
+
+// db.projects.udpate({_id: testProject._id}, {rootProjectId: testProjectRoot._id});
+
+// db.directories.insert({name: 'Sibling1', parentId: testProjectRoot._id});
+// db.directories.insert({name: 'Sibling2', parentId: testProjectRoot._id});
+// db.directories.insert({name: 'Sibling3', parentId: testProjectRoot._id});
