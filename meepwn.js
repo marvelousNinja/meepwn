830793bd31c4ff90cf3527eddd17ed8e7aba8d25
Alexander Sologub <alexander.sologub@rightscale.com>
2014-11-18 15:12:19 +0300

    ShareJS: Working with example

--- meepwn.js
+++ meepwn.js
@@ -1,23 +1,85 @@
+this.Documents = new Meteor.Collection("documents");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+  Template.docList.helpers({
+  documents: function() {
+    return Documents.find();
+  }
+});
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+Template.docList.events = {
+  "click button": function() {
+    return Documents.insert({
+      title: "untitled"
+    }, function(err, id) {
+      if (!id) {
+        return;
+      }
+      return Session.set("document", id);
+    });
+  }
+};
+
+Template.docItem.helpers({
+  current: function() {
+    return Session.equals("document", this._id);
+  }
+});
+
+Template.docItem.events = {
+  "click a": function(e) {
+    e.preventDefault();
+    return Session.set("document", this._id);
+  }
+};
+
+Template.docTitle.helpers({
+  title: function() {
+    var _ref;
+    return (_ref = Documents.findOne(this + "")) != null ? _ref.title : void 0;
+  }
+});
+
+Template.editor.helpers({
+  docid: function() {
+    return Session.get("document");
+  }
+});
+
+Template.editor.events = {
+  "keydown input[name=title]": function(e) {
+    var id;
+    if (e.keyCode !== 13) {
+      return;
     }
-  });
+    e.preventDefault();
+    $(e.target).blur();
+    id = Session.get("document");
+    return Documents.update(id, {
+      title: e.target.value
+    });
+  },
+  "click button": function(e) {
+    var id;
+    e.preventDefault();
+    id = Session.get("document");
+    Session.set("document", null);
+    return Meteor.call("deleteDocument", id);
+  }
+};
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+Template.editor.helpers({
+  cm: function() {
+    return Session.equals("editorType", "cm");
+  },
+  configCM: function() {
+    return function(cm) {
+      cm.setOption("theme", "default");
+      cm.setOption("lineNumbers", true);
+      cm.setOption("lineWrapping", true);
+      cm.setOption("smartIndent", true);
+      return cm.setOption("indentWithTabs", true);
+    };
+  }
+});
 }
