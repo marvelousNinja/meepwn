fcf06011de1f275bac9633b92fb97871eed8b7e2
Alexander Sologub <alexander.sologub@rightscale.com>
2014-12-06 00:15:25 +0300

    Links: Fix for project invitations link

--- levels/blocks/navigation/navigation__helpers.js
+++ levels/blocks/navigation/navigation__helpers.js
@@ -1,6 +1,6 @@
 if(Meteor.isClient) {
   Template.registerHelper('currentProject', function() {
-    if(Router.current().route.getName() === 'project') {
+    if(Router.current().route.getName() === 'projectsShow') {
       return Projects.findOne({ _id: Router.current().params._id });
     }
   });
